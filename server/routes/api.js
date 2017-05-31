e1e04532f94d2e343184ac868976f3e93c59968a
mtso <mtso@tuta.io>
2017-05-31 12:35:15 -0700

    Authenticate username in checkin route

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import request from 'superagent'
 import { attachToken, searchBusinesses } from '../controllers/yelp'
+import { validateParams, isNotCheckedIn, postCheckIn } from '../controllers/checkin'
+import { isAuthenticated } from '../controllers/auth'
 
 const api = express.Router()
 
@@ -16,5 +18,7 @@ api.get('/time', (req, res, next) => {
 })
 
 api.get('/search', attachToken, searchBusinesses)
+
+api.post('/checkin', isAuthenticated, validateParams, isNotCheckedIn, postCheckIn)
 
 export default api
