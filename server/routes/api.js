3b876ee602e3db1cf6da7e7a566a0db2b28f3d73
mtso <mtso@tuta.io>
2017-05-31 13:59:00 -0700

    Add cancelCheckin endpoint

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,24 +1,16 @@
 import express from 'express'
 import request from 'superagent'
 import { attachToken, searchBusinesses } from '../controllers/yelp'
-import { validateParams, isNotCheckedIn, postCheckIn } from '../controllers/checkin'
+import { validateParams, isNotCheckedIn, isCheckedIn, postCheckIn, cancelCheckin } from '../controllers/checkin'
 import { isAuthenticated } from '../controllers/auth'
 
 const api = express.Router()
 
-api.get('/test', (req, res) => {
-  res.json({
-    message: 'Hello~ from /test api route.',
-  })
-})
-
-api.get('/time', (req, res, next) => {
-  const now = new Date
-  res.json({ now })
-})
-
 api.get('/search', attachToken, searchBusinesses)
 
-api.post('/checkin', isAuthenticated, validateParams, isNotCheckedIn, postCheckIn)
+api.route('/checkin')
+  .use(isAuthenticated)
+  .post(validateParams, isNotCheckedIn, postCheckIn)
+  .delete(validateParams, isCheckedIn, cancelCheckIn)
 
 export default api
