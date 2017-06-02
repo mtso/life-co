73615a2ca00f832a6933a229a7ee54c41183836c
mtso <mtso@tuta.io>
2017-06-01 21:20:09 -0700

    Remove uriencoding

--- server/middleware.js
+++ server/middleware.js
@@ -6,14 +6,11 @@ import logger from 'morgan'
 import helmet from 'helmet'
 import passport from 'passport'
 import { Strategy as TwitterStrategy } from 'passport-twitter'
-import urlencode from 'urlencode'
-
-const callbackUrl = process.env.HOSTNAME + '/auth/twitter/callback'
 
 const twitterStrategy = new TwitterStrategy({
   consumerKey: process.env.TWITTER_CONSUMER_KEY,
   consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
-  callbackURL: urlencode(callbackUrl),
+  callbackURL: process.env.HOSTNAME + '/auth/twitter/callback',
 }, (token, tokenSecret, profile, done) => 
   done(null, {username: profile.username})
 )
